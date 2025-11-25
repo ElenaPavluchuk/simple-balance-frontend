75cb438e301bfccadf0e759319b7f820097d7d69
ElenaPavluchuk <elena.pvlch@gmail.com>
2025-11-25 12:23:22 +0300

    add-router-3: configure react-router

---
+++ src/app/providers/router/routeConfig/routeConfig.jsx
@@ -0,0 +1,8 @@
+import TestPage from "../../../../pages/TesePage";
+
+export const routeConfig = [
+  {
+    path: "/simple-balance-frontend/test",
+    element: <TestPage />,
+  },
+];
---
+++ src/pages/TesePage.jsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div>
+      <h1>I'm test page</h1>
+    </div>
+  );
+}
