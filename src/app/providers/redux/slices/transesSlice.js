ed91b648af16fa3bfa9c60da36020d76afd36541
ElenaPavluchuk <elena.pvlch@gmail.com>
2025-12-15 18:56:59 +0300

    feat-trans-15: add edit transactions feature

--- src/app/providers/redux/slices/transesSlice.js
+++ src/app/providers/redux/slices/transesSlice.js
@@ -12,10 +12,21 @@ export const transesSlice = createSlice({
     deleteTransFromRedux: (state, action) => {
       state.value = state.value.filter((trans) => trans.id !== action.payload);
     },
+    updateTransInRedux: (state, action) => {
+      const { id, name, amount, category, date } = action.payload;
+      const editedTrans = state.value.find((trans) => trans.id === id);
+      if (editedTrans) {
+        editedTrans.name = name;
+        editedTrans.amount = amount;
+        editedTrans.category = category;
+        editedTrans.date = date;
+      }
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { addTransToRedux, deleteTransFromRedux } = transesSlice.actions;
+export const { addTransToRedux, deleteTransFromRedux, updateTransInRedux } =
+  transesSlice.actions;
 
 export default transesSlice.reducer;
