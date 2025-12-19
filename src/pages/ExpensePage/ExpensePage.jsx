b1a5bd6b601e3d7248b57488d164dabe84b0f197
ElenaPavluchuk <elena.pvlch@gmail.com>
2025-12-19 21:20:18 +0300

    feat-trans-19: create transes list component

--- src/pages/ExpensePage/ExpensePage.jsx
+++ src/pages/ExpensePage/ExpensePage.jsx
@@ -7,8 +7,8 @@ import {
   deleteTransFromRedux,
   updateTransInRedux,
 } from "../../app/providers/redux/slices/transesSlice";
-import { Trash2, Pencil, X, Check } from "lucide-react";
 import { expenseCategories } from "../../shared/data/transCategories";
+import TransesList from "../../shared/ui/TransesList/TransesList";
 
 export default function ExpensePage() {
   const [openAddTransModal, setOpenAddTransModal] = useState(false);
@@ -90,103 +90,25 @@ export default function ExpensePage() {
           {transes
             .filter((trans) => trans.type === "expense")
             .map((trans) => (
-              <li
+              <TransesList
                 key={trans.id}
-                className="bg-white flex flex-row justify-between gap-10 rounded-2xl shadow-sm border border-gray-100 p-4 hover:shadow-md transition"
-              >
-                {editingTrans.id === trans.id ? (
-                  <>
-                    <div className="w-full">
-                      <div className="flex justify-between items-center ">
-                        <input
-                          value={editingTrans.name}
-                          name="name"
-                          onChange={handleEditInputChange}
-                          autoComplete="off"
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                        />
-                        <input
-                          type="number"
-                          value={editingTrans.amount}
-                          name="amount"
-                          onChange={handleEditInputChange}
-                          min="0"
-                          step="1"
-                          autoComplete="off"
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                        />
-                      </div>
-                      <div className="flex justify-between items-center mt-2 text-sm text-gray-500 ">
-                        <select
-                          value={editingTrans.category}
-                          onChange={handleEditInputChange}
-                          name="category"
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                        >
-                          <option value="">Select category</option>
-                          {expenseCategories.map((opt) => (
-                            <option key={opt.value} value={opt.value}>
-                              {opt.label}
-                            </option>
-                          ))}
-                        </select>
-                        <input
-                          type="date"
-                          value={editingTrans.date}
-                          name="date"
-                          onChange={handleEditInputChange}
-                          className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
-                        />
-                      </div>
-                    </div>
-                    <div className="flex gap-4 items-start">
-                      <Button
-                        onClick={() => saveUpdateTrans(trans.id)}
-                        size={4}
-                      >
-                        <Check className="text-gray-700" />
-                      </Button>
-                      <Button onClick={cancelUpdateTrans} size={4}>
-                        <X className="text-gray-700" />
-                      </Button>
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div className="w-full">
-                      <div className="flex justify-between items-center ">
-                        <p>{trans.name}</p>
-                        <p>{trans.amount}</p>
-                      </div>
-                      <div className="flex justify-between items-center mt-2 text-sm text-gray-500 ">
-                        <span className="bg-gray-100 px-2 py-1 rounded-lg">
-                          {trans.category}
-                        </span>
-                        <span>{trans.date}</span>
-                      </div>
-                    </div>
-                    <div className="flex flex-col gap-4 items-start">
-                      <Button
-                        onClick={() =>
-                          editTrans(
-                            trans.id,
-                            trans.name,
-                            trans.amount,
-                            trans.category,
-                            trans.date
-                          )
-                        }
-                        size={4}
-                      >
-                        <Pencil className="text-gray-700" />
-                      </Button>
-                      <Button onClick={() => deleteTrans(trans.id)} size={4}>
-                        <Trash2 className="text-gray-700" />
-                      </Button>
-                    </div>
-                  </>
-                )}
-              </li>
+                trans={trans}
+                editingTrans={editingTrans}
+                category={expenseCategories}
+                handleEditInputChange={handleEditInputChange}
+                saveUpdateTrans={() => saveUpdateTrans(trans.id)}
+                cancelUpdateTrans={cancelUpdateTrans}
+                editTrans={() =>
+                  editTrans(
+                    trans.id,
+                    trans.name,
+                    trans.amount,
+                    trans.category,
+                    trans.date
+                  )
+                }
+                deleteTrans={() => deleteTrans(trans.id)}
+              />
             ))}
         </ul>
       </div>
--- src/shared/ui/TransesList/TransesList.jsx
+++ src/shared/ui/TransesList/TransesList.jsx
@@ -1,1 +1,108 @@
-export default function TransesList() {}
+import PropTypes from "prop-types";
+import Button from "../Button/Button";
+import { Trash2, Pencil, X, Check } from "lucide-react";
+
+export default function TransesList({
+  trans,
+  editingTrans,
+  category,
+  handleEditInputChange,
+  saveUpdateTrans,
+  cancelUpdateTrans,
+  editTrans,
+  deleteTrans,
+}) {
+  return (
+    <li className="bg-white flex flex-row justify-between gap-10 rounded-2xl shadow-sm border border-gray-100 p-4 hover:shadow-md transition">
+      {editingTrans.id === trans.id ? (
+        <>
+          <div className="w-full">
+            <div className="flex justify-between items-center ">
+              <input
+                value={editingTrans.name}
+                name="name"
+                onChange={handleEditInputChange}
+                autoComplete="off"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+              <input
+                type="number"
+                value={editingTrans.amount}
+                name="amount"
+                onChange={handleEditInputChange}
+                min="0"
+                step="1"
+                autoComplete="off"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+            </div>
+            <div className="flex justify-between items-center mt-2 text-sm text-gray-500 ">
+              <select
+                value={editingTrans.category}
+                onChange={handleEditInputChange}
+                name="category"
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              >
+                <option value="">Select category</option>
+                {category.map((opt) => (
+                  <option key={opt.value} value={opt.value}>
+                    {opt.label}
+                  </option>
+                ))}
+              </select>
+              <input
+                type="date"
+                value={editingTrans.date}
+                name="date"
+                onChange={handleEditInputChange}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+              />
+            </div>
+          </div>
+          <div className="flex gap-4 items-start">
+            <Button onClick={saveUpdateTrans} size={4}>
+              <Check className="text-gray-700" />
+            </Button>
+            <Button onClick={cancelUpdateTrans} size={4}>
+              <X className="text-gray-700" />
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="w-full">
+            <div className="flex justify-between items-center ">
+              <p>{trans.name}</p>
+              <p>{trans.amount}</p>
+            </div>
+            <div className="flex justify-between items-center mt-2 text-sm text-gray-500 ">
+              <span className="bg-gray-100 px-2 py-1 rounded-lg">
+                {trans.category}
+              </span>
+              <span>{trans.date}</span>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 items-start">
+            <Button onClick={editTrans} size={4}>
+              <Pencil className="text-gray-700" />
+            </Button>
+            <Button onClick={deleteTrans} size={4}>
+              <Trash2 className="text-gray-700" />
+            </Button>
+          </div>
+        </>
+      )}
+    </li>
+  );
+}
+
+TransesList.PropTypes = {
+  trans: PropTypes.object.isRequired,
+  editingTrans: PropTypes.object.isRequired,
+  category: PropTypes.array.isRequired,
+  handleEditInputChange: PropTypes.func.isRequired,
+  saveUpdateTrans: PropTypes.func.isRequired,
+  cancelUpdateTrans: PropTypes.func.isRequired,
+  editTrans: PropTypes.func.isRequired,
+  deleteTrans: PropTypes.func.isRequired,
+};
