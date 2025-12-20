c4ab210e25e23ad7764a7fd8831e02bfb6b88aaa
ElenaPavluchuk <elena.pvlch@gmail.com>
2025-12-20 20:23:56 +0300

    feat-trans-19: create custom hook for transes functions

--- src/pages/ExpensePage/ExpensePage.jsx
+++ src/pages/ExpensePage/ExpensePage.jsx
@@ -2,52 +2,21 @@ import { useState } from "react";
 import Button from "../../shared/ui/Button/Button";
 import DialogModal from "../../shared/ui/DialogModal/DialogModal";
 import AddTransForm from "../../shared/ui/AddTransForm/AddTransForm";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  deleteTransFromRedux,
-  updateTransInRedux,
-} from "../../app/providers/redux/slices/transesSlice";
+import { useTransActions } from "../../shared/lib/customHooks/useTransActions";
 import { expenseCategories } from "../../shared/data/transCategories";
 import TransesList from "../../shared/ui/TransesList/TransesList";
 
 export default function ExpensePage() {
   const [openAddTransModal, setOpenAddTransModal] = useState(false);
-  const transes = useSelector((state) => state.transes.value);
-  const dispatch = useDispatch();
-  const [editingTrans, setEditingTrans] = useState(null);
-
-  const deleteTrans = (id) => {
-    dispatch(deleteTransFromRedux(id));
-  };
-
-  const editTrans = (trans) => {
-    setEditingTrans(trans);
-  };
-
-  const handleEditInputChange = (e) => {
-    const { name, value } = e.target;
-    setEditingTrans((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const saveUpdateTrans = () => {
-    if (!editingTrans) return;
-
-    const { id, name, amount, category, date } = editingTrans;
-    dispatch(
-      updateTransInRedux({
-        id,
-        name: name.trim(),
-        amount: Number(amount),
-        category,
-        date,
-      })
-    );
-    setEditingTrans(null);
-  };
-
-  const cancelUpdateTrans = () => {
-    setEditingTrans(null);
-  };
+  const {
+    transes,
+    editingTrans,
+    deleteTrans,
+    editTrans,
+    handleEditInputChange,
+    saveUpdateTrans,
+    cancelUpdateTrans,
+  } = useTransActions();
 
   return (
     <div className="flex flex-col gap-20">
