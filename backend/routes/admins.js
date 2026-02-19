f3c346770ca40131534b28ce22c7b1eaa0a5f51d
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-02-19 12:29:58 +0300

    api-admins-35:add get and update endpoints for exchange rates

--- backend/routes/admins.js
+++ backend/routes/admins.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { getUsers, deleteUser } = require("../controllers/adminController.js");
+const {
+  getUsers,
+  deleteUser,
+  getCurrencyRates,
+  updateCurrencyRates,
+} = require("../controllers/adminController.js");
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.delete("/users/:id", deleteUser);
+router.patch("/exchange-rates", updateCurrencyRates);
+router.get("/exchange-rates", getCurrencyRates);
 
 module.exports = router;
