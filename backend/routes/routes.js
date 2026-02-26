e8856f69b99119530c0dd05257f65f74e3aedbf2
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-02-26 14:14:52 +0300

    api-dashboard-37:add dashboard

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,7 @@ const auth = require("./auth.js");
 const users = require("./users.js");
 const transactions = require("./transactions.js");
 const admins = require("./admins.js");
+const dashboard = require("./dashboard.js");
 const authenticate = require("../middlewares/authentication.js");
 const verifyRole = require("../middlewares/authorization.js");
 
@@ -12,5 +13,6 @@ router.use("/auth", auth);
 router.use("/users", authenticate, users);
 router.use("/admins", authenticate, verifyRole("admin"), admins);
 router.use("/transactions", authenticate, transactions);
+router.use("/dashboard", dashboard);
 
 module.exports = router;
