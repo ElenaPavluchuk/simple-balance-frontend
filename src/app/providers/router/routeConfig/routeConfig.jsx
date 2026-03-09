9f3c4888048cd38da2dd5d2bb5cd0ad93794c0d8
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-03-09 12:18:41 +0300

    feat-layout-49:new layout

--- src/app/providers/router/routeConfig/routeConfig.jsx
+++ src/app/providers/router/routeConfig/routeConfig.jsx
@@ -1,11 +1,20 @@
-import AppLayout from "../../../ui/layout/AppLayout";
+// import AppLayout from "../../../ui/layout/AppLayout";
 import DashboardPage from "../../../../pages/DashboardPage/DashboardPage";
 import ExpensePage from "../../../../pages/ExpensePage/ExpensePage";
 import IncomePage from "../../../../pages/IncomePage/IncomePage";
+import Layout from "../../../../shared/ui/Layout/Layout";
 
 export const routeConfig = {
+  // path: "/",
+  // element: <AppLayout />,
+  // children: [
+  //   { index: true, element: <DashboardPage /> },
+  //   { path: "expense", element: <ExpensePage /> },
+  //   { path: "income", element: <IncomePage /> },
+  // ],
+
   path: "/",
-  element: <AppLayout />,
+  element: <Layout />,
   children: [
     { index: true, element: <DashboardPage /> },
     { path: "expense", element: <ExpensePage /> },
---
+++ src/shared/ui/Layout/Sidebar/config/data.js
@@ -0,0 +1,22 @@
+import { Home, Wallet, CreditCard } from "lucide-react";
+
+export const data = [
+  {
+    key: "dashboard",
+    label: "Dashboard",
+    icon: Home,
+    path: "/",
+  },
+  {
+    key: "expense",
+    label: "Expense",
+    icon: CreditCard,
+    path: "/expense",
+  },
+  {
+    key: "income",
+    label: "Income",
+    icon: Wallet,
+    path: "/income",
+  },
+];
