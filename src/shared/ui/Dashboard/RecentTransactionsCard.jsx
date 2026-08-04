ea128acd8cd3804ab50a41a44063487114cb71fe
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-08-04 10:36:12 +0300

    feat-dash-86:empty transaction card styling

--- src/shared/ui/Dashboard/RecentTransactionsCard.jsx
+++ src/shared/ui/Dashboard/RecentTransactionsCard.jsx
@@ -41,18 +41,27 @@ export default function RecentTransactionsCard({
         {navigateTo && (
           <Link
             to={navigateTo}
-            className="text-gray-600 hover:text-cyan-950 hover:underline text-base"
+            className="text-gray-600 hover:underline text-base"
           >
-            {transactions.length === 0 ? <CirclePlus size={20} /> : "View all"}
+            {transactions.length === 0 ? (
+              <CirclePlus
+                size={22}
+                className="hover:bg-emerald-800 hover:text-white rounded-full"
+              />
+            ) : (
+              "View all"
+            )}
           </Link>
         )}
       </div>
 
       {transactions.length === 0 && (
-        <div className="h-20 flex flex-col items-center justify-center">
-          <p className="text-sm">No transactions yet</p>
+        <div className="flex flex-col items-center justify-center min-h-90">
+          <p className="text-sm text-cyan-950">
+            No transactions in the last 30 days
+          </p>
           <p className="text-xs text-gray-300 mt-1">
-            Add your first transaction to see the list
+            Add transaction to see the list
           </p>
         </div>
       )}
