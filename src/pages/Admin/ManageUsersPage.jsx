f0a6f2bea4e2efe364c5720200d9247955d196be
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-08-12 18:36:59 +0300

    feat-admin-94:add mobile cards

--- src/pages/Admin/ManageUsersPage.jsx
+++ src/pages/Admin/ManageUsersPage.jsx
@@ -80,13 +80,70 @@ export default function ManageUsersPage() {
   return (
     <>
       <h2 className="text-3xl text-emerald-800">Manage Users</h2>
-      <p className="text-xs md:text-sm text-cyan-900 mt-1 mb-6">
+      <p className="text-xs md:text-sm text-cyan-900 mt-1 mb-6 text-balance">
         Administration tool for viewing and removing user accounts
       </p>
 
-      <Card>
+      {/* mobile cards */}
+      <div className="grid grid-cols-1 gap-4 md:hidden">
+        {users.map((user) => (
+          <Card key={user?.id} className="flex flex-row justify-between gap-5">
+            <div className="flex flex-col gap-3 w-full">
+              <div className="flex justify-between items-center">
+                <span className="text-sm font-medium text-cyan-900 inline-block w-12">
+                  Name
+                </span>
+                <span className="text-base font-semibold text-slate-900">
+                  {user?.user_name}
+                </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="text-sm font-medium text-cyan-900 inline-block w-12">
+                  Email
+                </span>
+                <span className="whitespace-nowrap text-base text-cyan-950">
+                  {user?.email}
+                </span>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="text-sm font-medium text-cyan-900 inline-block w-12">
+                  Role
+                </span>
+                <span
+                  className={`inline-block w-22 px-3 py-1 text-xs text-center font-semibold rounded-full ${user?.user_role === "ADMIN" ? "bg-red-100 text-red-800" : "bg-emerald-100 text-emerald-800"}`}
+                >
+                  {user?.user_role}
+                </span>
+              </div>
+            </div>
+
+            <span
+              title={
+                user?.user_role === "ADMIN"
+                  ? "Administrators cannot be removed"
+                  : "Delete user"
+              }
+              className={`w-8 h-8 border rounded-full flex items-center justify-center ${user?.user_role === "ADMIN" ? "border-gray-300 text-gray-400" : "border-emerald-800 text-emerald-800"}`}
+            >
+              <Button
+                variant="icon"
+                onClick={() => {
+                  setSelectedUserId(user?.id);
+                  setOpenDialogModal(true);
+                }}
+                disabled={user?.user_role === "ADMIN"}
+              >
+                <Trash size={18} />
+              </Button>
+            </span>
+          </Card>
+        ))}
+      </div>
+
+      {/* table */}
+      <Card className="hidden md:block">
         <table className="min-w-full divide-y divide-emerald-600">
-          <thead>
+          <thead className="">
             <tr>
               <th className="px-6 py-4 text-left text-xs font-bold text-emerald-800 uppercase">
                 Name
@@ -131,6 +188,7 @@ export default function ManageUsersPage() {
                         ? "Administrators cannot be removed"
                         : "Delete user"
                     }
+                    className={`flex justify-center max-w-7 ${user?.user_role === "ADMIN" ? "text-gray-400 cursor-not-allowed" : "text-emerald-700 hover:text-emerald-500"}`}
                   >
                     <Button
                       variant="icon"
@@ -138,7 +196,6 @@ export default function ManageUsersPage() {
                         setSelectedUserId(user?.id);
                         setOpenDialogModal(true);
                       }}
-                      className={`${user?.user_role === "ADMIN" ? "text-gray-400 cursor-not-allowed" : "text-emerald-700 hover:text-emerald-500"}`}
                       disabled={user?.user_role === "ADMIN"}
                     >
                       <Trash size={18} />
