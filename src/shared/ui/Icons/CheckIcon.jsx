3ff77eb6765dc835fb921cd76aef2bfb22532b80
ElenaPavluchuk <elena.pvlch@gmail.com>
2026-08-13 18:46:36 +0300

    feat-menu-96:transaction cards icons

---
+++ src/shared/ui/Icons/CheckIcon.jsx
@@ -0,0 +1,20 @@
+export default function CheckIcon({ className }) {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M0.857422 14.6572L5.53742 20.6744C5.69417 20.8777 5.89459 21.0433 6.12389 21.1588C6.35319 21.2743 6.60552 21.3369 6.86222 21.3418C7.11893 21.3467 7.37347 21.2939 7.60705 21.1873C7.84062 21.0807 8.04726 20.923 8.21171 20.7258L23.1431 2.65723"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
